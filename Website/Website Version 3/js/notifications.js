708696bde7bf5a2613bdbfe02e0b4d9e6bf8b8cb
Fraham <frahamp@gmail.com>
2015-01-27 21:52:49 +0000

    able to add a new row to the table

---
+++ Website/Website Version 3/js/notifications.js
@@ -0,0 +1,7 @@
+var newRow = function(){
+  $('table').append('<tbody><tr class="odd gradeX"><td>Trident</td><td>Internet Explorer 4.0</td><td>Win 95+</td><td class="center">4</td><td class="center">X</td></tr></tbody>');
+}
+
+$(document).ready(function () {
+  newRow();
+});
