9d175f4f8bd6ebb478c4844b249bf0621ea00427
Fraham <frahamp@gmail.com>
2015-02-18 01:11:42 +0000

    local website able to connect to database now

--- Website/htdocs/pages/connectToDatabase.php
+++ Website/htdocs/pages/connectToDatabase.php
@@ -1,11 +1,12 @@
 <?php	
 	$servername = "localhost";
+	$hostname = "easyLiving.ml";
 	$username = "root";
 	$password = "cheeseBurger";
 	$dbname = "easyliving";
 
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($hostname, $username, $password, $dbname);
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
