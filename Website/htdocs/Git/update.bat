928e658b58fd775d7994ca4e4603ddae7abe7284
jeisonas <jeisonas@gmail.com>
2015-02-18 22:42:08 +0000

    Git update fully mirrors repo

--- Website/htdocs/Git/update.bat
+++ Website/htdocs/Git/update.bat
@@ -1,3 +1,4 @@
 @echo off
 cd C:\Users\easyLiving\Desktop\EasyLiving
-"C:\Program Files (x86)\Git\cmd\git" pull
+"C:\Program Files (x86)\Git\cmd\git" fetch --all
+"C:\Program Files (x86)\Git\cmd\git" reset --hard origin/master
