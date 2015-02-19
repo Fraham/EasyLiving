7b3b2053bc8a360c771c3a97bc735b681938630d
Fraham <frahamp@gmail.com>
2015-02-19 11:37:33 +0000

    moved new sensor

--- Website/htdocs/pages/addData.php
+++ Website/htdocs/pages/addData.php
@@ -7,4 +7,17 @@
 
     $conn->close();
   }
+
+  function addNewSensor($sensorID, $name, $messageOn, $messageOff, $roomID)
+  {
+    require "../connect.php";
+
+    $statement = $conn->prepare("INSERT INTO sensors (sensorID, name, messageOn, messageOff, roomID) VALUES (?, ?, ?, ?, ?)");
+
+    $statement->bind_param("isssi", $sensorID, $name, $messageOn, $messageOff, $roomID);
+
+    addNewData($statement);
+
+    $conn->close();
+  }
 ?>
