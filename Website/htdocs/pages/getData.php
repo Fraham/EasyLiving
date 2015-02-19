e89a906dbd1fa3b0d1c2527d52e61ca3e021e7fb
Graham Pye <frahamp@gmail.com>
2015-02-19 14:40:48 +0000

    getNotifications added, not tested

--- Website/htdocs/pages/getData.php
+++ Website/htdocs/pages/getData.php
@@ -8,6 +8,27 @@
     $conn->close();
   }
 
+  function getNotifications($sensorID, $startDate, $endDate)
+  {
+    $statement = "SELECT COUNT(*) as Amount, DATE(date) as Date, hour(date) as Time
+	   FROM log
+     WHERE log.sensorID = $sensorID
+     AND date BETWEEN '$startDate' AND '$endDate'
+     GROUP BY YEAR(date), MONTH(date), DAY(date), Time
+     ORDER BY log.logID
+     DESC LIMIT 1000;";
 
-  
+    $result = $conn->query($statement);
+
+    if ($result->num_rows > 0)
+    {
+      while($row = $result->fetch_assoc())
+      {
+
+      }
+    }
+
+    $conn->close();
+  }
+
 ?>
