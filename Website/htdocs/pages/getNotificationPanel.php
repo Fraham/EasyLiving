19bfcc5c0277640fb5a94654d2be3a2087f01047
Graham Pye <frahamp@gmail.com>
2015-02-19 13:27:53 +0000

    panel now has time

--- Website/htdocs/pages/getNotificationPanel.php
+++ Website/htdocs/pages/getNotificationPanel.php
@@ -1,18 +1,20 @@
-<?php	
+<?php
 	$tableHtml = "";
-	require "../connect.php";
-	
-	//$sql = "SELECT DATE_FORMAT(time,'%k:%i:%s') as time, data FROM test ORDER BY idtest DESC LIMIT 15";
-	$sql = "SELECT data FROM test ORDER BY idtest DESC LIMIT 10";
-	
+	require_once "../connect.php";
+
+	$sql = "SELECT DATE_FORMAT(time,'%k:%i:%s') as time, data FROM test ORDER BY idtest DESC LIMIT 10";
+	//$sql = "SELECT data FROM test ORDER BY idtest DESC LIMIT 10";
+
 	$result = $conn->query($sql);
 
-	if ($result->num_rows > 0) 
+	if ($result->num_rows > 0)
 	{
 		while($row = $result->fetch_assoc()) {
 			$tableHtml .= "<a href='#' class='list-group-item'>";//<i class='fa fa-comment fa-fw'></i>";
 			$tableHtml .= "$row[data]";
-			$tableHtml .= "<span class='pull-right text-muted small'><em>4 minutes ago</em></span>";
+			$tableHtml .= "<span class='pull-right text-muted small'><em>";
+			$tableHtml .= "$row[time]";
+			$tableHtml .= "</em></span>";
 		}
 	}
 	$conn->close();
