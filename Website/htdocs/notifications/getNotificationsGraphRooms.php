111126089c09d9f3fc4ee4e331762f68853844d0
Fraham <frahamp@gmail.com>
2015-02-24 20:15:37 +0000

    basic function, needs html

---
+++ Website/htdocs/notifications/getNotificationsGraphRooms.php
@@ -0,0 +1,22 @@
+<?php
+	$roomList = "";
+	require_once "../src/connect.php";
+
+  $houseID = "111111";
+
+  $statement = "SELECT dName FROM room WHERE room.houseID = $houseID";
+
+	$result = $conn->query($statement);
+
+	if ($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			//$roomList .= ""; html here
+		}
+	}
+
+	$conn->close();
+
+	echo $roomList;
+?>
