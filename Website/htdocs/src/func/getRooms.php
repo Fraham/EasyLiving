b72b6827f7cac7a7f70e5b3b52fe8e0fb6bcaca5
Fraham <frahamp@gmail.com>
2015-02-24 18:44:57 +0000

    formatting

--- Website/htdocs/src/func/getRooms.php
+++ Website/htdocs/src/func/getRooms.php
@@ -1,61 +1,61 @@
 <?php
-    $roomHTML = "";
-    require_once "../connect.php";
+$roomHTML = "";
+require_once "../connect.php";
 
-    $statement = "SELECT dName FROM room";
-    //WHERE room.houseID = $houseID";
+$statement = "SELECT dName FROM room";
+//WHERE room.houseID = $houseID";
 
-    $result = $conn->query($statement);
+$result = $conn->query($statement);
 
-    $count = 0;
+$count = 0;
 
-    if ($result->num_rows > 0)
+if ($result->num_rows > 0)
+{
+  while($row = $result->fetch_assoc())
+  {
+    $count++;
+    $roomHTML .= "<div class='col-lg-6 col-md-6'>
+    <div class='panel panel-red'>
+    <div class='panel-heading'>
+    <div class='row'>
+    <div class='col-xs-3'>
+    <i class='fa fa-comments fa-4x'></i>
+    </div>
+    <div class='col-xs-9 text-right'>
+    <div class='huge'>";
+    $roomHTML .= "$row[dName]";
+
+    $roomHTML .= "</div>
+    <div>Occupied</div>
+    </div>
+    </div>
+    </div>
+    <div class='panel-body'>
+    <div class='col-md-6'>
+    <h4><font color='black'>Window: </font><span class='text-danger'>Open</span></h4>
+    </div>
+    <div class='col-md-6'>
+    <h4>Lamp:
+    <span><div class='btn-group btn-toggle'>
+    <button class='btn btn-xs btn-default'>ON</button>
+    <button class='btn btn-xs btn-danger active'>OFF</button>
+    </span>
+    </h4>
+    </div>
+    </select>
+    </div>
+    </div>
+    </div>
+    </div>";
+
+    if ($count == 2)
     {
-      while($row = $result->fetch_assoc())
-      {
-        $count++;
-        $roomHTML .= "<div class='col-lg-6 col-md-6'>
-            <div class='panel panel-red'>
-            <div class='panel-heading'>
-            <div class='row'>
-            <div class='col-xs-3'>
-            <i class='fa fa-comments fa-4x'></i>
-            </div>
-            <div class='col-xs-9 text-right'>
-            <div class='huge'>";
-        $roomHTML .= "$row[dName]";
-
-        $roomHTML .= "</div>
-            <div>Occupied</div>
-            </div>
-            </div>
-            </div>
-            <div class='panel-body'>
-            <div class='col-md-6'>
-            <h4><font color='black'>Window: </font><span class='text-danger'>Open</span></h4>
-            </div>
-            <div class='col-md-6'>
-            <h4>Lamp:
-			<span><div class='btn-group btn-toggle'> 
-			<button class='btn btn-xs btn-default'>ON</button>
-			<button class='btn btn-xs btn-danger active'>OFF</button>
-			</span>
-			</h4>
-			</div>
-            </select>
-            </div>
-            </div>
-            </div>
-            </div>";
-
-            if ($count == 2)
-            {
-              $roomHTML .= "<div class='clearfix'></div>";
-              $count = 0;
-            }
-      }
+      $roomHTML .= "<div class='clearfix'></div>";
+      $count = 0;
     }
+  }
+}
 
-    $conn->close();
-    echo $roomHTML;
+$conn->close();
+echo $roomHTML;
 ?>
