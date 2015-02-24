cf8191c028f39ee55241ca9dc715e05f39177b35
jeisonas <jeisonas@gmail.com>
2015-02-24 00:21:16 +0000

    Working template and the dashboard

--- Website/htdocs/dashboard/index.php
+++ Website/htdocs/dashboard/index.php
@@ -1,173 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Easy Living</title>
-        <!-- jQuery -->
-        <script src="../src/bower_components/jquery/dist/jquery.min.js"></script>
-  <link rel="shortcut icon" href="../src/images/TabLogo.png">
-  <!-- Bootstrap Core CSS -->
-  <link href="../src/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- MetisMenu CSS -->
-  <link href="../src/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-  <!-- Timeline CSS -->
-  <link href="../src/dist/css/timeline.css" rel="stylesheet">
-
-  <!-- Custom CSS -->
-  <link href="../src/dist/css/sb-admin-2.css" rel="stylesheet">
-
-  <!-- Morris Charts CSS -->
-  <link href="../src/bower_components/morrisjs/morris.css" rel="stylesheet">
-
-  <!-- Custom Fonts -->
-  <link href="../src/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-</head>
-
-<body>
-
-  <div id="wrapper">
-
-    <!-- Navigation -->
 <?php
-	require_once "../src/template.php";
-	$title	= 'Dashboard';
+	 $title = "Dashboard";
+	 $path = "../src/templates/";
+	 include $path."main.php";
 ?>
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-    <div id="page-wrapper">
-      <div class="row">
-        <div class="col-lg-12">
-          <h1 class="page-header">Dashboard</h1>
-        </div>
-        <!-- /.col-lg-12 -->
-      </div>
-      <!-- /.row -->
-      <div class="row">
-		<div class="col-lg-9">
-			<div class="col-lg-12">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<i class="fa fa-home fa-fw"></i> House Overview
-					</div>
-					<!-- /.panel-heading -->
-					<div class="panel-body">
-					<h4>Front Door: <span class="text-success">Closed</span></h4>
-					<h4>Back Door: <span class="text-success">Closed</span></h4>
-					<h4>House Occupied: <span class="text-warning">Yes</span></h4>
-
-					</div>
-            <!-- /.panel-body -->
-				</div>
+<div class="col-lg-9"> 
+	<div class="col-lg-12">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<i class="fa fa-home fa-fw"></i> House Overview
+			</div>
+			<!-- /.panel-heading -->
+			<div class="panel-body">
+				<h4>Front Door: <span class="text-success">Closed</span></h4>
+				<h4>Back Door: <span class="text-success">Closed</span></h4>
+				<h4>House Occupied: <span class="text-warning">Yes</span></h4>
 
 			</div>
-          <!-- /#page-wrapper -->
-        <div class="col-lg-12" id = "roomsPanel">
-        <script>
-          refresh();
-          function refresh()
-          {
-            $.post( "../src/func/getRooms.php", function( data ) {
-              $( "#roomsPanel" ).html( data );
-            });
-          }
-          var intervalID = setInterval(refresh, 500);
-        </script>
+			<!-- /.panel-body -->
+		</div>
+
+	</div>
+	<!-- /#page-wrapper -->
+	<div class="col-lg-12" id = "roomsPanel">
+		<script>
+			refresh();
+			function refresh()
+			{
+				$.post( "../src/func/getRooms.php", function( data ) {
+					$( "#roomsPanel" ).html( data );
+				});
+			}
+			var intervalID = setInterval(refresh, 500);
+		</script>
 
 		<div class="clearfix visible-md-block visible-lg-block"></div>
 
 
 		<div class="clearfix visible-md-block"></div>
 
-	 </div>
-	 </div>
-	<div class="col-lg-3 col-md-12">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<i class="fa fa-bell fa-fw"></i> Notifications Panel
-			</div>
-			<!-- /.panel-heading -->
-			<div class="panel-body">
-				<div class="list-group" id="notificationPanel">
-					<script>
-						refresh();
-						function refresh()
-						{
-							$.post( "getNotificationPanel.php", function( data ) {
-							  $( "#notificationPanel" ).html( data );
-							});
-						}
-						var intervalID = setInterval(refresh, 500);
-					</script>
-				</div>
-				<!-- /.list-group -->
-				<a href="notifications.php" class="btn btn-default btn-block">View All Alerts</a>
-			</div>
-			<!-- /.panel-body -->
+	</div>
+</div>
+<div class="col-lg-3 col-md-12">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<i class="fa fa-bell fa-fw"></i> Notifications Panel
 		</div>
-
-	</div>
-          <!-- /#page-wrapper -->
-	<script>	
-		$('.btn-toggle').click(function() {
-			$(this).find('.btn').toggleClass('active');  
-    
-			if ($(this).find('.btn-danger').size()>0) {
-				$(this).find('.btn').toggleClass('btn-danger');
-			}
-			$(this).find('.btn').toggleClass('btn-default');
-		});
-
-		$('form').submit(function(){
-			alert($(this["options"]).val());
-			return false;
-		});
-	</script>
-
-	</div>
-	</div>
+		<!-- /.panel-heading -->
+		<div class="panel-body">
+			<div class="list-group" id="notificationPanel">
+				<script>
+					refresh();
+					function refresh()
+					{
+						$.post( "getNotificationPanel.php", function( data ) {
+							$( "#notificationPanel" ).html( data );
+						});
+					}
+					var intervalID = setInterval(refresh, 500);
+				</script>
+			</div>
+			<!-- /.list-group -->
+			<a href="/notifications" class="btn btn-default btn-block">View All Alerts</a>
+		</div>
+		<!-- /.panel-body -->
 	</div>
 
+</div>
+<script>	
+	$('.btn-toggle').click(function() {
+		$(this).find('.btn').toggleClass('active');  
 
+		if ($(this).find('.btn-danger').size()>0) {
+			$(this).find('.btn').toggleClass('btn-danger');
+		}
+		$(this).find('.btn').toggleClass('btn-default');
+	});
 
+	$('form').submit(function(){
+		alert($(this["options"]).val());
+		return false;
+	});
+</script>
 
-        <!-- Bootstrap Core JavaScript -->
-        <script src="../src/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-        <!-- Metis Menu Plugin JavaScript -->
-        <script src="../src/bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-
-        <!-- Custom Theme JavaScript -->
-        <script src="../src/dist/js/sb-admin-2.js"></script>
-
-      </body>
-
-      </html>
+<?php include $path."footer.php"; ?>
---
+++ Website/htdocs/src/templates/main.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+		<title><?php echo $title; ?></title>
+
+		<link rel='shortcut icon' href='images/TabLogo.png'> <!-- favicon -->
+		<script src='<?php echo $path; ?>../bower_components/jquery/dist/jquery.min.js'></script>
+		<link href='<?php echo $path; ?>../bower_components/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../bower_components/metisMenu/dist/metisMenu.min.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../dist/css/timeline.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../dist/css/sb-admin-2.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../bower_components/morrisjs/morris.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../bower_components/font-awesome/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+		<script src='<?php echo $path; ?>../bower_components/bootstrap/dist/js/bootstrap.min.js'></script>
+		<script src='<?php echo $path; ?>../bower_components/metisMenu/dist/metisMenu.min.js'></script>
+		<script src='<?php echo $path; ?>../dist/js/sb-admin-2.js'></script>
+		
+		<div id='wrapper'>
+			<nav class=' navbar navbar-default navbar-static-top' role='navigation' style='margin-bottom: 0'>
+				<div class='navbar-header centered'>
+					<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
+						<span class='sr-only'>Toggle navigation</span>
+						<span class='icon-bar'></span>
+						<span class='icon-bar'></span>
+						<span class='icon-bar'></span>
+					</button>
+					<div class ='clearfix'></div>
+					<img src='<?php echo $path; ?>../images/logoFlat.png' class='hidden-lg hidden-sm hidden-md img-responsive' style='width:35%;height:35%; margin:auto; padding:0px'>
+				</div>
+				<ul class='nav navbar-top-links navbar-right pull-right'>
+					<li class='dropdown'>
+						<a class='dropdown-toggle' style='color:#D80000' data-toggle='dropdown' href='#'>
+							<i class='fa fa-user fa-fw'></i>  <i class='fa fa-caret-down'></i>
+						</a>
+						<ul class='dropdown-menu dropdown-user'>
+							<div style='text-align: center'><h4> Available Houses</h4></div>
+
+							<li class='divider'></li>
+							<p style = 'text-align: center'>******php to pull all households to available******</p>
+							<li class='divider'></li>
+							<li><a href='#'><i class='fa fa-plus fa-fw'></i> Add House</a></li>
+							<li><a href='../login'><i class='fa fa-sign-out fa-fw'></i> Logout</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+			<div class='navbar-default sidebar' role='navigation'>
+				<div class='sidebar-nav navbar-collapse'>
+					<ul class='nav' id='side-menu'>
+						<div class='input-group custom-search-form col-lg-12 hidden-xs'>
+							<img src='<?php echo $path; ?>../images/logo.png' class='img-responsive' style=''>
+						</div>
+						<?php include 'getMenu.php';?>
+					</ul>
+				</div>
+			</div>
+			
+
+			<div id='page-wrapper'>
+				<div class='row'>
+					<div class='col-lg-12'>
+						<h1 class='page-header'><?php echo $title; ?></h1>
+					</div>
+				</div>
+	</head>
+	<body>
