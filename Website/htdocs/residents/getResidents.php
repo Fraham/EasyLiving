6da20a455109be97d4ba513cce8254e9a44b6e7a
Graham Pye <frahamp@gmail.com>
2015-02-26 13:08:27 +0000

    displays residents

---
+++ Website/htdocs/residents/getResidents.php
@@ -0,0 +1,30 @@
+<?php
+require_once "../src/connect.php";
+
+$residents = "";
+
+$statement = "SELECT U.email, H.dName FROM users as U
+    INNER JOIN user_households as UH
+    ON UH.userID = U.userID
+    INNER JOIN house as H
+    ON UH.houseID = H.houseID
+    WHERE	UH.houseID = $houseID";
+
+$result = $conn->query($statement);
+
+if ($result->num_rows > 0)
+{
+  while($row = $result->fetch_assoc())
+  {
+    $residents .= "<tbody>";
+    $residents .= "<tr class='odd gradeX'>";
+    $residents .= "<td class='center'> $row[email] </td>";
+    $residents .= "<td class='center'> $row[dName] </td>";
+    $residents .= "</tr>";
+    $residents .= "</tbody>";
+  }
+}
+
+$conn->close();
+echo $residents;
+?>
--- Website/htdocs/residents/index.php
+++ Website/htdocs/residents/index.php
@@ -10,33 +10,15 @@
                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                   <thead>
                     <tr>
-                      <th>Rendering engine</th>
-                      <th>Browser</th>
-                      <th>Platform(s)</th>
-                      <th>Engine version</th>
-                      <th>CSS grade</th>
+                      <th>Email</th>
+                      <th>Current Household</th>
                     </tr>
                   </thead>
-                  <tbody>
-                    <tr class="odd gradeX">
-                      <td>Trident</td>
-                      <td>Internet Explorer 4.0</td>
-                      <td>Win 95+</td>
-                      <td class="center">4</td>
-                      <td class="center">X</td>
-                    </tr>
-                  </tbody>
-                  <tbody>
-                    <tr class="odd gradeX">
-                      <td>Trident</td>
-                      <td>Internet Explorer 4.0</td>
-                      <td>Win 95+</td>
-                      <td class="center">4</td>
-                      <td class="center">X</td>
-                    </tr>
-                  </tbody>
+									<?php
+										$houseID = "111111";
+										include "getResidents.php";
+									?>
                 </div>
-
           </div>
         </div>
 <?php
