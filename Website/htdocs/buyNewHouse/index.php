11b1ffeb09f7e21c7210c02d9c1434342d5c7f71
InghamNick <inghamnick@yahoo.co.uk>
2015-03-06 16:28:46 +0000

    buy new property layout finished

--- Website/htdocs/buyNewHouse/index.php
+++ Website/htdocs/buyNewHouse/index.php
@@ -10,10 +10,20 @@
 	<div class = "panel panel-danger">
 		<div class = "panel-heading"> <h2> New Property Preferences </h2> </div>
 		<div class = "panel-body">
-			<form>
-				<label>Property Name:*</label>
-				<input class ="form-control col-lg-6">
-			</form>
+			 <form method="post" name="registration_form" action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>">
+                Property Name: <input type="text"
+                                name="pName"
+                                id="pName"
+                                class="form-control danger"/><br>
+                Password: <input type="password"
+                                 name="password"
+                                 id="password"
+                                 class="form-control"
+                                 /><br>
+                Confirm password: <input type="password"
+                                         name="confirmpwd"
+                                         id="confirmpwd"
+                                         class="form-control" /><br>
 		</div>
 	</div>
 </div>
@@ -22,7 +32,27 @@
 		<div class = "panel panel-danger">
 			<div class = "panel-heading"> <h2> Payment Details </h2> </div>
 			<div class = "panel-body">
+				<form method="post" name="registration_form" action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>">
+                Name on Card: <input type="text"
+                                name="cardName"
+                                id="email"
+                                class="form-control"
+                                placeholder="Card Holder's Name"/><br>
+                Card Number: <input type="text"
+								 name="cardNo" 
+                                 class="form-control"
+                                 /><br>
+                Expiration Date: <br> <input  class="btn btn-default " readonly placeholder="Month"  id="month">
+                				 <input  class="btn btn-default " readonly placeholder="Year"  id="year">                
+                
+				<br>
+				<br>
+                Card CVV: <input type="password	"
+                                         name="confirmpwd"
+                                         id="confirmpwd"
+                                         class="form-control" /><br>
 
+                </form>
 			</div>
 		</div>
 	</div>
@@ -32,6 +62,21 @@
 		<button type="button" class="btn btn-lg btn-danger pull-right" >Submit</button>
 	</div>
 </div>
+
+<script type="text/javascript">
+	$('#month').datepicker({
+		format: 'MM (mm)',
+		viewMode: "months", 
+    	minViewMode: "months",
+		autoclose: true,
+	});
+	$('#year').datepicker({
+		format: 'yyyy',
+		viewMode: "years", 
+    	minViewMode: "years",
+		autoclose: true,
+	});
+</script>
 
 <?php include $path."footer.php"?>
 
