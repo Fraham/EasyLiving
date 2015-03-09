78f9521dfb4ab9b386f8e44dddf20f020d455a0e
Graham Pye <frahamp@gmail.com>
2015-03-09 10:58:31 +0000

    fixed the dashboard

--- Website/htdocs/dashboard/getDoors.php
+++ Website/htdocs/dashboard/getDoors.php
@@ -21,27 +21,27 @@
         {
             if (0 === strpos($row['sensorID'], '02'))
             {
-                if (strcmp($row['state'], "open") === 0)
+                if ($row['state'] == 0)
                 {
-                    $doorHTML .= "<h4>";
-                    $doorHTML .= "$row[name]";
-                    $doorHTML .= ": <span class='text-danger'>Open</span></h4>";
+									$doorHTML .= "<h4>";
+									$doorHTML .= "$row[name]";
+									$doorHTML .= ": <span class='text-success'>Closed</span></h4>";
                 }
                 else
                 {
-                    $doorHTML .= "<h4>";
-                    $doorHTML .= "$row[name]";
-                    $doorHTML .= ": <span class='text-success'>Closed</span></h4>";
+									$doorHTML .= "<h4>";
+									$doorHTML .= "$row[name]";
+									$doorHTML .= ": <span class='text-danger'>Open</span></h4>";
                 }
             }
             elseif (0 === strpos($row['sensorID'], '01'))
             {
-                if (strcmp($row['state'], "detected") === 0)
+                if ($row['state'] == 1)
                 {
                     $motion = 1;
                 }
             }
-						
+
 		}
 	}
     else
