e9b2e2afe24213430e36ac3d9f475319c415d4dc
Graham Pye <frahamp@gmail.com>
2015-03-12 15:45:27 +0000

    got notifications graph thing working

--- Website/htdocs/notifications/getNotificationsGraphRooms.php
+++ Website/htdocs/notifications/getNotificationsGraphRooms.php
@@ -2,11 +2,15 @@
 	$roomList = "";
 	require_once "../src/connect.php";
 
+	include "../src/includes/functions.php";
+
+	sec_session_start();
+
 	$userID = $_SESSION['user_id'];
 
-	/*$houseID = $_SESSION['house_id'];
+	//$houseID = $_SESSION['house_id'];
 
-  $statement = "SELECT dName FROM room WHERE room.houseID = $houseID";*/
+  //$statement = "SELECT dName FROM room WHERE room.houseID = $houseID";
 
 	$statement = "SELECT dName FROM room
 								INNER JOIN user_households
@@ -23,6 +27,10 @@
       $roomList .= "$row[dName]";
       $roomList .= "</option>";
 		}
+	}
+	else
+	{
+		$roomList .= "<option>No rooms $userID</option>";
 	}
 
 	$conn->close();
