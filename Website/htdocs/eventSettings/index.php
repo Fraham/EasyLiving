f8379c1a9f0d1fd6604d5db532b2e43a8252920a
InghamNick <inghamnick@yahoo.co.uk>
2015-03-16 15:55:01 +0000

    events page fixed

--- Website/htdocs/eventSettings/index.php
+++ Website/htdocs/eventSettings/index.php
@@ -42,32 +42,32 @@
 	
 						for ($i = 1; $i <= $loopvalue; $i++) {
 						echo '
-									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
-										<select class="form-control">
-											<option selected hidden>Any</option>
-												<option>Any</option>
-											<?php
-												getRooms();
-											?>
-										</select>
-									</div>
-									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
-										<select class="form-control">
-											<option selected hidden>---</option>
-											<?php
-												getSensors();
-											?>
-										</select>
-									</div>
-									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
-										<input type="number" min="0" class="form-control" placeholder="Period of time">
-									</div>';
+						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
+							<select class="form-control">
+								<option selected hidden>Any</option>
+									<option>Any</option>
+								<?php
+									getRooms();
+								?>
+							</select>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
+							<select class="form-control">
+								<option selected hidden>---</option>
+								<?php
+									getSensors();
+								?>
+							</select>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
+							<input type="number" min="0" class="form-control" placeholder="Period of time">
+						</div>';
 						}
 					?>
 		</div>
 	</div>
 	</div>
-	<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add Notification Type</button>
+	<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add Event</button>
 		
 	<?php include $path."footer.php"?>
 
