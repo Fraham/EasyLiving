cd933455a40810fce81d6bf3077922298a229d33
Josh Andersen <josh.andersen94@gmail.com>
2015-03-30 21:52:30 +0100

    Template event conditions & settings page

--- Website/htdocs/eventSettings/index.php
+++ Website/htdocs/eventSettings/index.php
@@ -11,6 +11,10 @@
 				<a><div class="panel-heading" ></a>
 				Fridge Door Buzzer
 				</div>
+				<div class="panel-body"id="settingsBody">
+					<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add sensor</button>
+					<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add device to be activated</button>
+				</div>
 			</div>
 		</div>
 		<div class="col-lg-12">
@@ -19,56 +23,13 @@
 				Fridge Door Notification
 				</div>
 				<div class="panel-body"id="settingsBody">
-					<div class="col-lg-6">
-					<label>Number of Sensors</label>
-					<input type="number" id="noOfSensors" min="0" class="form-control" placeholder="Choose number of sensors">
-					</div>
-					<div class="col-lg-6">
-						<button class = "btn btn-danger" style="margin-top:24px">Submit</button>
-					</div>
-
-					<div class="col-lg-12" style="margin-top:10px">
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-							<label>Sensor Location</label>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-							<label>Sensor</label>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-							<label>Timer:</label>
-						</div>
-					<?php 
-						$loopvalue = 3;
-	
-						for ($i = 1; $i <= $loopvalue; $i++) {
-						echo '
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
-							<select class="form-control">
-								<option selected hidden>Any</option>
-									<option>Any</option>
-								<?php
-									getRooms();
-								?>
-							</select>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
-							<select class="form-control">
-								<option selected hidden>---</option>
-								<?php
-									getSensors();
-								?>
-							</select>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="margin-top:10px">
-							<input type="number" min="0" class="form-control" placeholder="Period of time">
-						</div>';
-						}
-					?>
+					<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add sensor</button>
+					<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add device to be activated </button>
+				</div>
+			</div>
 		</div>
+	<button class = "btn btn-danger btn-lg" style="margin-top:30px">Create New Event</button>
 	</div>
-	</div>
-	<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add Event</button>
-		
 	<?php include $path."footer.php"?>
 
 <?php else : ?>
--- Website/htdocs/settings/index.php
+++ Website/htdocs/settings/index.php
@@ -12,49 +12,27 @@ $(document).ready(function(){
 });
 </script>
 
-<div class="tabbable">
-  <ul class="nav nav-tabs">
-    <li><a href="#pane1" data-toggle="tab">User Preferences	</a></li>
-    <li class="active"><a href="#pane2" data-toggle="tab">General Account Settings</a></li>
-  </ul>
-  <div class="tab-content">
-    <div id="pane1" class="tab-pane">
-    	<h3 class="page-header">User Preferences</h3>
-      	<div class="col-lg-6">
-			<label>Notification Type:</label>
-			<select class="form-control">
-				<option>Push Notifications</option>
-				<option>Email Notifications</option>
-				<option>Email and Push Notifications</option>
-			</select>
-		</div>
-		<div class="clearfix"></div>
-		<div class="col-lg-6" style="margin-top:20px;">
-				<label>Keep all Sensor and Event data for:</label>
-				<select class="form-control">
-					<option>1 month</option>
-					<option>3 months</option>
-					<option>6 months</option>
-					<option>9 months</option>
-					<option>12 months</option>
-					<option>18 months</option>
-					<option>24 months</option>
-				</select>
-		</div>
-    </div>
-    <div id="pane2" class="tab-pane active">
-    	<h3 class="page-header">General Account Settings</h3>
-		</div>
-		<div class="col-lg-4" style="margin-bottom:10px;">
-			<strong>Email:</strong>
-		</div>
-		<div class="col-lg-4" style="margin-bottom:10px;" >
-			<input type="Text" class="form-control" placeholder="" disabled>
-		</div>
-		<div class="col-lg-2 col-md-3">
-			<button type="button" class="btn btn-lg btn-danger" style="margin-bottom:10px;" data-toggle="modal" data-target="#EditEmailModal">Edit</button>
-		</div>
-		<div class="clearfix"></div>
+ <h3 class="page-header">User Preferences</h3>
+    <div class="col-lg-6">
+		<label>Notification Type:</label>
+		<select class="form-control">
+			<option>Push Notifications</option>
+			<option>Email Notifications</option>
+			<option>Email and Push Notifications</option>
+		</select>
+	</div>
+    	
+	<h3 class="page-header">General Account Settings</h3>
+	<div class="col-lg-4" style="margin-bottom:10px;">
+		<strong>Email:</strong>
+	</div>
+	<div class="col-lg-4" style="margin-bottom:10px;" >
+		<input type="Text" class="form-control" placeholder="" disabled>
+	</div>
+	<div class="col-lg-2 col-md-3">
+		<button type="button" class="btn btn-lg btn-danger" style="margin-bottom:10px;" data-toggle="modal" data-target="#EditEmailModal">Edit</button>
+	</div>
+	<div class="clearfix"></div>
 		<div class="col-lg-4" style="margin-bottom:10px;">
 			<strong>Password:</strong>
 		</div>
@@ -65,36 +43,35 @@ $(document).ready(function(){
 			<button type="button" class="btn btn-lg btn-danger" data-toggle="modal" data-target="#EditPasswordModal">Edit</button>
 		</div>
     </div>
-  </div><!-- /.tab-content -->
-</div><!-- /.tabbable -->
+
 
 <div class="modal fade" id="EditPasswordModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-					<h2 class="modal-title" id="myModalLabel">Change Password</h2>
-          </div>
-          <div class="modal-body row">
-            <div class="form-group col-lg-12" style="margin:10px">
-               <form action="../src/includes/process_login.php" method="post" name="login_form">
-                <label>Old Password:</label> <input type="password" id="oldPassword" class="form-control"/>
-                <br>
-                <label>New Password:</label> <input type="password" id="newPassword" class="form-control"/>
-				<br>
-                <label>Confirm Password:</label> <input type="password" id="confirmPassword" class="form-control"/>
-				<br>
-                <input type="button"
+				<h2 class="modal-title" id="myModalLabel">Change Password</h2>
+			</div>
+			<div class="modal-body row">
+				<div class="form-group col-lg-12" style="margin:10px">
+					<form action="../src/includes/process_login.php" method="post" name="login_form">
+					<label>Old Password:</label> <input type="password" id="oldPassword" class="form-control"/>
+					<br>
+					<label>New Password:</label> <input type="password" id="newPassword" class="form-control"/>
+					<br>
+					<label>Confirm Password:</label> <input type="password" id="confirmPassword" class="form-control"/>
+					<br>
+					<input type="button"
                        value="Confirm"
                        class="btn btn-lg btn-danger btn-block"
                        onclick="formhash(this.form, this.form.password);" />
-                <input type="button"
+					<input type="button"
                        value="Cancel"
                        class="btn btn-lg btn-danger btn-block"
                        data-dismiss="modal" aria-hidden="true" />
-				</form>
+					</form>
+				</div>
 			</div>
 		</div>
-	</div>
 	</div>
 </div>
 <div class="modal fade" id="EditEmailModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
