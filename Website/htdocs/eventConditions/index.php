44a1cbdb1f55b2c127dc1ac7f61fe2a3cfa5c790
Graham Pye <frahamp@gmail.com>
2015-05-19 18:37:31 +0100

    removed unneeded code

--- Website/htdocs/eventConditions/index.php
+++ Website/htdocs/eventConditions/index.php
@@ -20,21 +20,6 @@
 				$event->getEventFormat();
 			}
 		?>
-		
-		<div class="col-lg-12">
-			<div class="panel panel-default">
-				<a><div class="panel-heading" ></a>
-				Fridge Door Notification
-				</div>
-				<div class="panel-body"id="settingsBody">
-					<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add sensor</button>
-					<br>
-					<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add condition</button>
-					<br>
-					<button class = "btn btn-danger btn-lg" style="margin-top:30px">Add device to be activated </button>
-				</div>
-			</div>
-		</div>
 	<button class = "btn btn-danger btn-lg" style="margin-top:30px">Create New Event</button>
 	</div>
 	<?php include $path."footer.php"?>
