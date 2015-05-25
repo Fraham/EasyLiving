d06180352b1efa255b3b18a71a1ac0f82ade1a17
Graham Pye <frahamp@gmail.com>
2015-05-25 20:19:32 +0100

    delete rooms, sends sensor to right room

--- Website/htdocs/rooms/deleteRoom.php
+++ Website/htdocs/rooms/deleteRoom.php
@@ -2,8 +2,43 @@
 	if (isset($_POST["func"]) and isset($_POST["id"]))
 	{
 		require "../src/connect.php";
-
+		
 		$id = intval($_POST['id']);
+		
+		$statement2 = "SELECT houseID
+					FROM room
+					WHERE roomID = $id";
+					
+		$result = $conn->query($statement2);
+
+		if ($result->num_rows > 0)
+		{
+			$row = $result->fetch_assoc();
+			
+			$houseID = $row["houseID"];
+			
+			$statement3 = "SELECT roomID
+							FROM room
+							WHERE room.houseID = '$houseID'
+							AND room.dName = 'Unallocated Sensors'";
+							
+			$result = $conn->query($statement3);
+
+			if ($result->num_rows > 0)
+			{
+				$row = $result->fetch_assoc();
+				
+				$newRoomID = $row["roomID"];
+			
+				$statement4 = "UPDATE sensors
+						SET roomID = '$newRoomID'
+						WHERE roomID = $id";
+						
+				$result = $conn->query($statement4);
+			}
+		}
+
+		
 		$statement = "DELETE FROM room WHERE roomID = $id";
 
 		if (!$conn->query($statement)) {
