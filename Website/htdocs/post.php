cbecb3d793e79e7f30356b6e175e58c4a8b15e08
Graham Pye <frahamp@gmail.com>
2015-05-25 18:31:21 +0100

    system check

--- Website/htdocs/post.php
+++ Website/htdocs/post.php
@@ -15,6 +15,54 @@
 		}
 		else if ($_POST["msg"] == "allow")
 			$sql = "UPDATE sensors SET assigned = 2 WHERE sensorID = '".$_POST['id']."' AND assigned = 1;";
+		else if ($_POST["msg"] == "sysCheck")
+		{
+			$getHouseStatement = "SELECT house.houseID
+                		FROM house
+		                INNER JOIN room
+		                ON house.roomID = room.roomID
+		                INNER JOIN sensors
+		                ON sensors.roomID = room.roomID
+		                WHERE sensors.sensorID = {$_POST['id']}";
+						
+			$getHouseResult = $conn->query($showStatement);
+				
+			if ($getHouseResult->num_rows > 0)
+			{
+				$row = $getHouseResult->fetch_assoc();
+				
+				$houseID = $row['houseID'];
+			}
+						
+			$statement = "SELECT sensors.name, room.dName
+                		FROM sensors
+		                INNER JOIN room
+		                ON sensors.roomID = room.roomID
+		                INNER JOIN house
+		                ON room.houseID = house.houseID
+		                WHERE house.houseID = $houseID
+						AND sensors.sensorID LIKE '02%'
+		                AND state = 1";
+						
+			$result = $conn->query($statement);
+			
+			$onMessage = "";
+				
+			if ($result->num_rows > 0)
+			{
+				$row = $getHouseResult->fetch_assoc();
+				
+				$onMessage = "{$row['name']} - {$row['dName']}";
+			}
+			else
+			{
+				$onMessage = "Secure";
+			}
+			
+			$sql = "UPDATE sensors
+					SET (messageOn = '$onMessage', done = '0')
+					WHERE sensors.sensorID = {$_POST['id']}";
+		}
 
 		if (!$conn->query($sql)) 
 			echo "Error: " . $sql . "<br>" . $conn->error;
