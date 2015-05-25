8e14dc343c7076a87f41130abb06aad5df201308
Josh Andersen <josh.andersen94@gmail.com>
2015-05-25 22:54:26 +0100

    and again

--- Website/htdocs/sensors/index.php
+++ Website/htdocs/sensors/index.php
@@ -118,7 +118,7 @@
 									</select>
 								</div>
 							</div>							
-							<input type="button" value="Edit Sensor" class="btn btn-lg btn-danger btn-block" onclick="editSensor()" />
+							<input type="button" value="Confirm" class="btn btn-lg btn-danger btn-block" onclick="editSensor()" />
 							<input type="button" value="Cancel" class="btn btn-lg btn-danger btn-block" data-dismiss="modal" aria-hidden="true" />
 						</form>
 					</div>
