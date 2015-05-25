c10662f0a5560453ec44f4e259846d2fa8b88160
Josh Andersen <josh.andersen94@gmail.com>
2015-05-25 15:12:06 +0100

    do not get motion detected

--- Website/htdocs/dashboard/getNotificationPanel.php
+++ Website/htdocs/dashboard/getNotificationPanel.php
@@ -16,6 +16,7 @@
 					INNER JOIN house
 					ON room.houseID = house.houseID
 					WHERE house.houseID = $houseID
+					AND LEFT(sensors.sensorID, 2) != 01
           ORDER BY logID
 					DESC LIMIT 10";
 
