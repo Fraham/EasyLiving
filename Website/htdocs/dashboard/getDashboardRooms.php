371788db27f062f73afe7bb705b16e0095da3de2
Graham Pye <frahamp@gmail.com>
2015-05-25 14:47:33 +0100

    displays the rooms on the dashboard

--- Website/htdocs/dashboard/getDashboardRooms.php
+++ Website/htdocs/dashboard/getDashboardRooms.php
@@ -1,138 +1,143 @@
 <?php
 require_once "../src/connect.php";
-$roomHTML = "";
+require_once "../src/classes/PropertyClass.php";
 
 if (!isset($blockSize))
 	$blockSize = 370;
 
-if (isset($_SESSION['house_id']))
+$userID = $_SESSION['user_id'];
+    
+$properties = [];
+    
+$properties = Property::getByUserID($userID);
+
+$roomHTML = "";
+    
+foreach($properties as $property)
 {
-
-	$houseID = $_SESSION['house_id'];
-
 	$statement = "SELECT R.dName, R.roomID, RC.occupied, RC.unoccupied, I.icon, R.roomID
-									FROM room as R
-									INNER JOIN room_colour as RC
-									ON R.colourID = RC.colourID
-									INNER JOIN icons as I
-									ON R.iconID = I.iconID
-									WHERE	R.houseID = $houseID";
-
+				FROM room as R
+				INNER JOIN room_colour as RC
+				ON R.colourID = RC.colourID
+				INNER JOIN icons as I
+				ON R.iconID = I.iconID
+				INNER JOIN user_room
+				ON user_room.roomID = R.roomID
+				WHERE R.houseID = $property->houseID
+				AND user_room.showRoom = '1'
+				AND user_room.userID = $userID";
+				
 	$result = $conn->query($statement);
 
 	if ($result->num_rows > 0)
 	{
 		while($row = $result->fetch_assoc())
 		{
-			$unallocated= "Unallocated Sensors";
-			if(strcmp($row['dName'],$unallocated)==0){
-			}
-			else{
-			
-			$occupiedStatement = "SELECT state, sensorID FROM sensors
-				INNER JOIN room
-				ON sensors.roomID = room.roomID
-				WHERE room.roomID = $row[roomID]";
-
-			$occupiedResult = $conn->query($occupiedStatement);
-
-			$motion = 0;
-
-			if ($occupiedResult->num_rows > 0)
-			{
-				while($occupiedRow = $occupiedResult->fetch_assoc())
+			if (strcmp($row['dName'], "Unallocated Sensors") == 0)
+			{}
+ 			else
+			{			
+				$occupiedStatement = "SELECT state, sensorID FROM sensors
+					INNER JOIN room
+					ON sensors.roomID = room.roomID
+					WHERE room.roomID = $row[roomID]";
+	
+				$occupiedResult = $conn->query($occupiedStatement);
+	
+				$motion = 0;
+	
+				if ($occupiedResult->num_rows > 0)
 				{
-					if (0 === strpos($occupiedRow['sensorID'], '01'))
-		            {
-		                if ($occupiedRow['state'] == 1)
-		                {
-		                    $motion = 1;
-		                }
-		            }
+					while($occupiedRow = $occupiedResult->fetch_assoc())
+					{
+						if (0 === strpos($occupiedRow['sensorID'], '01'))
+			            {
+			                if ($occupiedRow['state'] == 1)
+			                {
+			                    $motion = 1;
+			                }
+			            }
+					}
 				}
-			}
-
-			if ($motion == 1) //motion sensor state
-			{
-				$color = $row["occupied"];
-				$state = "Occupied";
-			}
-			else
-			{
-				$color = $row["unoccupied"];
-				
-				$lastSeenStatement = "SELECT date 
-				FROM log
-				INNER JOIN sensors
-				On sensors.sensorID = log.sensorID
-				INNER JOIN room
-				ON sensors.roomID = room.roomID
-				WHERE room.roomID = $row[roomID] and sensors.sensorID LIKE '01%'
-				ORDER BY logID DESC
-				LIMIT 1";
-				
-				$lastSeenResult = $conn->query($lastSeenStatement);
-				
-				if ($lastSeenResult->num_rows > 0)
+	
+				if ($motion == 1) //motion sensor state
 				{
-					$lastSeenRow = $lastSeenResult->fetch_assoc();
-					
-					$state = "Motion last detected at: ";
-					
-					$theDate = strtotime($lastSeenRow['date']);
-					
-					$state .= date("h:ia l d", $theDate);
+					$color = $row["occupied"];
+					$state = "Occupied";
 				}
 				else
 				{
-					$state = "No motion sensor detected";
+					$color = $row["unoccupied"];
+					
+					$lastSeenStatement = "SELECT date 
+					FROM log
+					INNER JOIN sensors
+					On sensors.sensorID = log.sensorID
+					INNER JOIN room
+					ON sensors.roomID = room.roomID
+					WHERE room.roomID = $row[roomID] and sensors.sensorID LIKE '01%'
+					ORDER BY logID DESC
+					LIMIT 1";
+					
+					$lastSeenResult = $conn->query($lastSeenStatement);
+					
+					if ($lastSeenResult->num_rows > 0)
+					{
+						$lastSeenRow = $lastSeenResult->fetch_assoc();
+						
+						$state = "Motion last detected at: ";
+						
+						$theDate = strtotime($lastSeenRow['date']);
+						
+						$state .= date("h:ia l d", $theDate);
+					}
+					else
+					{
+						$state = "No motion sensor detected";
+					}			
+				}
+				
+				include_once ("{$path}../classes/SensorClass.php");
+				
+				$sensorHTML = "";
+				
+				$sensors = [];
+	
+				$sensors = Sensor::getByRoomID($row['roomID']);
+	
+				foreach ($sensors as $sensor)
+				{
+					$sensorHTML .= $sensor->getBlockFormat();
 				}			
-			}
-			
-			include_once ("{$path}../classes/SensorClass.php");
-			
-			$sensorHTML = "";
-			
-			$sensors = [];
-
-			$sensors = Sensor::getByRoomID($row['roomID']);
-
-			foreach ($sensors as $sensor)
-			{
-				$sensorHTML .= $sensor->getBlockFormat();
-			}			
-
-			$roomHTML .= <<<HTML
-			<div class='col-lg-2 room-xs' style='width: {$blockSize}px; margin: auto; float: none;display: inline-block;'>
-				<div class='panel panel-{$color}'>
-					<div class='panel-heading'>
-						<div class='row'>
-							<div class='col-xs-3'>
-								<i class='fa fa-{$row["icon"]} fa-4x'></i>
+	
+				$roomHTML .= <<<HTML
+				<div class='col-lg-2 room-xs' style='width: {$blockSize}px; margin: auto; float: none;display: inline-block;'>
+					<div class='panel panel-{$color}'>
+						<div class='panel-heading'>
+							<div class='row'>
+								<div class='col-xs-3'>
+									<i class='fa fa-{$row["icon"]} fa-4x'></i>
+								</div>
+								<div class='col-xs-9 text-right'>
+									<div class='huge' name=''>{$row["dName"]}</div>
+									<div>{$state}</div>
 							</div>
-							<div class='col-xs-9 text-right'>
-								<div class='huge' name=''>{$row["dName"]}</div>
-								<div>{$state}</div>
 						</div>
 					</div>
-				</div>
-				<div class='panel-body'>
-					{$sensorHTML}
+					<div class='panel-body'>
+						{$sensorHTML}
+					</div>
 				</div>
 			</div>
-		</div>
 HTML;
-		}
+			}
 		}
 	}
+}
 
-	$conn->close();
-	echo $roomHTML;
-}
-else
-{
-	echo "house id not set";
-}
+$conn->close();
+echo $roomHTML;
+
 ?>
 
 <script>
