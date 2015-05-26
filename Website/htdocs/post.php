97ef8ff9a1fb252284fca7c9d3bfda68691e6776
Alex Shleifman <jeisonas@gmail.com>
2015-05-26 08:58:09 +0100

    request thing

--- Website/htdocs/post.php
+++ Website/htdocs/post.php
@@ -5,6 +5,36 @@
 
 		if (isset($_POST["temp"]))
 			$sql = "INSERT INTO tempHum (sensorID, temp, hum) VALUES (".$_POST['id'].", ".$_POST['temp'].", ".$_POST['hum'].")";
+		else if ($_POST["msg"] == "request")
+		{
+			$houseID = getHouseID();
+
+			$sql = "SELECT sensorID	FROM sensors WHERE done = 0";
+
+			if (!$conn->query($sql)) 
+				echo "Error: " . $sql . "<br>" . $conn->error;
+
+			$todo = "";
+			while ($row = $sql->fetch_assoc()) 
+			{
+				if (substr($row['sensorID'], 0, 2) === "08")
+					$query = "SELECT messageOn FROM sensors WHERE sensorID = ".$row['sensorID']."";
+				else
+					$query = "SELECT state FROM sensors WHERE sensorID = ".$row['sensorID']."";
+
+				if (!$conn->query($query)) 
+					echo "Error: " . $query . "<br>" . $conn->error;
+				$row2 = $query->fetch_assoc();
+				$msg = $row2['messageOn'] || $row2['state'];
+				$todo .= $row['sensorID'] + $msg + ";";
+			}
+
+
+
+			echo $todo;
+
+			return 0;
+		}
 		else if(strlen($_POST["msg"]) == 1)
 		{
 			$statement = "UPDATE sensors SET state = '".$_POST['msg']."' WHERE sensorID = '".$_POST['id']."';";
@@ -19,22 +49,8 @@
 			$sql = "UPDATE sensors SET assigned = 2 WHERE sensorID = '".$_POST['id']."' AND assigned = 1;";
 		else if ($_POST["msg"] == "sysCheck")
 		{
-			$getHouseStatement = "SELECT house.houseID
-                		FROM house
-		                INNER JOIN room
-		                ON house.houseID = room.houseID
-		                INNER JOIN sensors
-		                ON sensors.roomID = room.roomID
-		                WHERE sensors.sensorID = '".$_POST['id']."'";
-						
-			$getHouseResult = $conn->query($getHouseStatement);
-				
-			if ($getHouseResult->num_rows > 0)
-			{
-				$row = $getHouseResult->fetch_assoc();
-				
-				$houseID = $row['houseID'];
-			}
+			
+			$houseID = getHouseID();
 						
 			$statement = "SELECT sensors.name, room.dName
                 		FROM sensors
@@ -70,6 +86,27 @@
 			echo "Error: " . $sql . "<br>" . $conn->error;
 
 		$conn->close();
+	}
+
+	function getHouseID()
+	{
+		$getHouseStatement = "SELECT house.houseID
+							  FROM house
+							  INNER JOIN room
+							  ON house.houseID = room.houseID
+							  INNER JOIN sensors
+							  ON sensors.roomID = room.roomID
+							  WHERE sensors.sensorID = '".$_POST['id']."'";
+
+		$getHouseResult = $conn->query($getHouseStatement);
+
+		if ($getHouseResult->num_rows > 0)
+		{
+			$row = $getHouseResult->fetch_assoc();
+
+			$houseID = $row['houseID'];
+		}
+		return $houseID
 	}
 
 	function display()
