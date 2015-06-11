fb12618ba6d1e37b4a7da410544d44286bfe24f4
Fraham <frahamp@gmail.com>
2015-06-12 00:50:50 +0100

    using inheritance for cleaner code

--- Website/htdocs/src/classes/SensorClass.php
+++ Website/htdocs/src/classes/SensorClass.php
@@ -43,17 +43,33 @@ class Sensor
 	
 		if ($result->num_rows > 0)
 		{
-		  while($row = $result->fetch_assoc())
-		  {
-		    $sensor = new Sensor;
-		    $sensor->sensorID     = $row['sensorID'];
-		    $sensor->name    		= $row['name'];
-		    $sensor->messageOn	= $row['messageOn'];
-		    $sensor->messageOff   = $row['messageOff'];
-		 	$sensor->roomID    	= $row['roomID'];
-		  	$sensor->state    	= $row['state'];
-		
-		    $sensors[] = $sensor;
+			while($row = $result->fetch_assoc())
+			{
+				if (substr( $row['sensorID'], 0, 2) === "01")// motion sensor
+				{
+					$sensor = new MotionSensor;
+				}
+				else if (substr( $row['sensorID'], 0, 2) === "02")// door sensor
+				{
+					$sensor = new DoorSensor;
+				}
+				else if (substr( $row['sensorID'], 0, 2) === "05")// relay sensor
+				{
+					$sensor = new RelaySensor;
+				}
+				else
+				{
+					$sensor = new Sensor;
+				}
+				
+				$sensor->sensorID = $row['sensorID'];
+				$sensor->name		= $row['name'];
+				$sensor->messageOn	= $row['messageOn'];
+				$sensor->messageOff   = $row['messageOff'];
+				$sensor->roomID	= $row['roomID'];
+				$sensor->state	= $row['state'];
+				
+				$sensors[] = $sensor;
 		  }
 		}
 		
@@ -64,43 +80,6 @@ class Sensor
 	{
 		$sensorBlock = "";
 		
-		if (substr( $this->sensorID, 0, 2) === "01") // motion sensor
-		{
-			
-		}
-		if (substr( $this->sensorID, 0, 2) === "02")// door sensor
-		{
-			$message = "";
-			
-			if(strcmp($this->state, "0") === 0)
-			{
-				$message = $this->messageOff;
-			}
-			else
-			{
-				$message = $this->messageOn;
-			}
-			
-			$sensorBlock = <<<HTML
-				<div class='col-md-6'>
-					<h4><font color='black'>{$this->name}: </font><span><strong>{$message}</strong></span></h4>
-				</div>
-HTML;
-		}
-		if (substr( $this->sensorID, 0, 2) === "05")// door sensor
-		{
-			$sensorBlock = <<<HTML
-			<div class='col-md-12'>
-				<h4><font color='black'>{$this->name}: </font></h4>
-				<div class='col-md-6'>
-					<input type="button" value="On" class="btn btn-sm btn-danger btn-block" onclick="turnOn('{$this->sensorID}')" />
-				</div>
-				<div class='col-md-6'>
-					<input type="button" value="Off" class="btn btn-sm btn-danger btn-block" onclick="turnOff('{$this->sensorID}')" />
-				</div>
-			</div>
-HTML;
-		}
 		return $sensorBlock;
 	}
 	
@@ -263,6 +242,64 @@ HTML;
 	}
 }
 
+class MotionSensor extends Sensor
+{
+	public function getBlockFormat()
+	{
+		$sensorBlock = "";
+		
+		return $sensorBlock;
+	}
+}
+
+class DoorSensor extends Sensor
+{
+	public function getBlockFormat()
+	{
+		$sensorBlock = "";
+		
+		$message = "";
+			
+		if(strcmp($this->state, "0") === 0)
+		{
+			$message = $this->messageOff;
+		}
+		else
+		{
+			$message = $this->messageOn;
+		}
+			
+		$sensorBlock = <<<HTML
+			<div class='col-md-6'>
+				<h4><font color='black'>{$this->name}: </font><span><strong>{$message}</strong></span></h4>
+			</div>
+HTML;
+		
+		return $sensorBlock;
+	}
+}
+
+class RelaySensor extends Sensor
+{
+	public function getBlockFormat()
+	{
+		$sensorBlock = "";
+		
+		$sensorBlock = <<<HTML
+		<div class='col-md-12'>
+			<h4><font color='black'>{$this->name}: </font></h4>
+			<div class='col-md-6'>
+				<input type="button" value="On" class="btn btn-sm btn-danger btn-block" onclick="turnOn('{$this->sensorID}')" />
+			</div>
+			<div class='col-md-6'>
+				<input type="button" value="Off" class="btn btn-sm btn-danger btn-block" onclick="turnOff('{$this->sensorID}')" />
+			</div>
+		</div>
+HTML;
+		
+		return $sensorBlock;
+	}
+}
 
 
 ?>
