71e6068bf7fa5902d2744a946b55e122a2676108
Fraham <frahamp@gmail.com>
2015-07-09 22:40:22 +0100

    adding in the where clauses

--- Website/htdocs/notifications/history.php
+++ Website/htdocs/notifications/history.php
@@ -10,6 +10,37 @@
 	
 	function updateSensorsList()
 	{
+		require "../src/connect.php";
 		
+		session_start();
+
+		$userID = $_SESSION['user_id'];
+	
+		$where = " WHERE user_households.userID = ";
+		$where .= $userID;
+	
+		if(isset($_POST['propertyID']) && isset($_POST['roomID']))
+		{
+			$propertyID = $_POST['propertyID'];
+			$roomID = $_POST['roomID'];
+			
+			if ($propertyID !== "Any")
+			{
+				$where .= " AND room.houseID = ";	
+				$where .= $propertyID;
+			}
+			if ($roomID !== "Any")
+			{
+				$where .= " AND sensors.roomID = ";
+				$where .= $roomID;
+			}
+		}
+	
+		$statement = "SELECT sensors.name, sensorID FROM sensors
+		INNER JOIN room
+		ON room.roomID = sensors.roomID
+		INNER JOIN user_households
+		ON user_households.houseID = room.houseID";  
+		$statement .= $where;
 	}
 ?>
