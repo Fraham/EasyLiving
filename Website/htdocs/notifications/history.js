5269912c2f3b2c9d06541081c9a6d0bbd8fd4f47
Fraham <frahamp@gmail.com>
2015-07-09 20:54:30 +0100

    made a new js file for all the js code for the history page

---
+++ Website/htdocs/notifications/history.js
@@ -0,0 +1,42 @@
+$(function () {
+  $("#propertySelect").change(function()
+  {
+    var houseID = $('#propertySelect').val();
+
+    var url = "?propertyID=" + houseID + "&option=room";
+
+    $.post("getNotificationsGraph.php" + url, function( data )
+    {
+      $("#roomSelect").html( data );
+    });
+
+    url = "?propertyID=" + houseID + "&option=sensor";
+
+    $.post("getNotificationsGraph.php" + url, function( data )
+    {
+      $("#sensorSelect").html( data );
+    });
+  });
+  
+
+
+  $( "#roomSelect" ).change(function()
+  {
+    var roomID = $('#roomSelect').val();
+
+    var url = "?roomID=" + roomID;
+
+    $.post("getNotificationsGraph.php" + url, function( data )
+    {
+      $("#sensorSelect").html( data );
+    });
+  });
+
+
+
+  $( "#sensorSelect" ).change(function() {
+    var ID= $('#sensorSelect').val();
+    var type = substr();
+    //alert( "Handler for .change() called." );
+  });
+});
