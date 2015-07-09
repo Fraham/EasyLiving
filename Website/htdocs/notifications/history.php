3d9950c6dd2f830ec80a665d103fe8f455c4f7ff
Fraham <frahamp@gmail.com>
2015-07-09 22:12:26 +0100

    adding in a function selector

--- Website/htdocs/notifications/history.php
+++ Website/htdocs/notifications/history.php
@@ -1,3 +1,15 @@
 <?php
+	if(isset($_POST['action']) && !empty($_POST['action'])) 
+	{
+	    $action = $_POST['action'];
+	    switch($action) 
+		{
+	        case 'updateSensorsList' : updateSensorsList();break;
+	    }
+	}
 	
+	function updateSensorsList()
+	{
+		
+	}
 ?>
