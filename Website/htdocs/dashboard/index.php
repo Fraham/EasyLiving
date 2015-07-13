6a5af627a0f41e36b720bfaec6d6a0438fa47a0e
Fraham <frahamp@gmail.com>
2015-07-13 15:08:36 +0100

    moving the js to a seperate file

--- Website/htdocs/dashboard/index.php
+++ Website/htdocs/dashboard/index.php
@@ -6,101 +6,7 @@
 
 <?php if (login_check($conn) == true) : ?>
 <script src="getInfo.js"></script>
-<script>
-	var date = new Date("July 21, 1983 01:15:00");
-	var newDate = ISODateString(date);
-
-	$( document ).ready(function() {
-		var refreshAjax;
-		refresh();
-		getOverview();
-	});
-
-	$( window ).unload(function() {
-  		refreshAjax.abort();
-	});
-
-	window.onunload = unloadPage;
-	function unloadPage()
-	{
-	    refreshAjax.abort();
-	}
-
-	$(window).bind('beforeunload',function(){
-		refreshAjax.abort();
-	});
-
-	function reloadPage()
-	{
-		refreshAjax.abort();
-		var date = new Date("July 21, 1983 01:15:00");
-		newDate = ISODateString(date);
-	}
-
-	function refresh()
-	{
-		refreshAjax = $.ajax({
-			url: 'getNotificationPanel.php',
-			dataType: 'json',
-			async:true,
-			data: {
-				'date':newDate
-			},
-			timeout: 30000,
-			cache: false,
-			success: function(result)
-			{
-				var data = result;
-
-				if (data['newData'] === "yes")
-				{
-					var html = "";
-
-					var htmlData = data['data'];
-
-					for(var j = 0; j < htmlData.length; j++)
-	      			{
-						html += "<a href='../notifications' class='list-group-item'>";
-						html += htmlData[j]['name'] + " - " + htmlData[j]['message'];
-						html += "<span class='pull-right text-muted small'><em>";
-						html += htmlData[j]['time'];
-						html += "</em></span>";
-					}
-
-					$("#notificationPanel").html(html);
-
-					date = new Date($.now());
-					newDate = ISODateString(date);
-				}
-			},
-			error: function(e){
-				//console.log(e);
-			},
-			complete: function(){
-
-
-				refresh();
-			}
-		});
-	}
-
-	function ISODateString(d)
-	{
-  		function pad(n)
-		{
-			  return n<10 ? '0'+n : n
-		}
-
-	 	 return d.getUTCFullYear()+'-'
-	      + pad(d.getUTCMonth()+1)+'-'
-	      + pad(d.getUTCDate()) +' '
-	      + pad(d.getUTCHours() + 1)+':'
-	      + pad(d.getUTCMinutes())+':'
-	      + pad(d.getUTCSeconds())
-	}
-
-
-</script>
+<script src="dashboard.js"></script>
 
 <div class="row">
 	<div class="col-md-9 col-sm-12">
@@ -145,24 +51,6 @@
 
 
 </div>
-
-<script>
-		function turnOn(sensorID)
-		{
-			$.post("turnOn.php", { sensorID: sensorID })
-			.done(function( data ) {
-				location.reload();
-			});
-		};
-		function turnOff(sensorID)
-		{
-			$.post("turnOff.php", { sensorID: sensorID })
-			.done(function( data ) {
-				location.reload();
-			});
-		};
-
-</script>
 
 <?php include $path."footer.php"; ?>
 
