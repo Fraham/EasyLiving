a99ffdf49eef7199d0bf5352e1738b5c1d6e4c10
InghamNick <inghamnick@yahoo.co.uk>
2015-10-27 19:28:15 +0000

    printing call of function as text fix

--- Website/htdocs/spotHistory/getSpots.php
+++ Website/htdocs/spotHistory/getSpots.php
@@ -12,12 +12,13 @@ if ($result->num_rows > 0)
 {
   while($row = $result->fetch_assoc())
   {
+   $temp = getTemp($row[spotID]);
     $spots .= "<tbody>";
     $spots .= "<tr class='odd gradeX'>";
     $spots .= "<td class='center'> $row[spotID] </td>";
     $spots .= "<td class='center'> $row[zone] </td>";
     $spots .= "<td class='center'> 0 </td>";
-    $spots .= "<td class='center'> getTemp($row[spotID]) </td>";
+    $spots .= "<td class='center'> $temp </td>";
     $spots .= "<td class='center'> <button>History</button> </td>";
     
     
