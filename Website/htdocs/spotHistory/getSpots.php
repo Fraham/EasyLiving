ccd5e4244a720b9cfed375877e220fce2e9c0d1a
InghamNick <inghamnick@yahoo.co.uk>
2015-10-27 20:25:21 +0000

    fixing...

--- Website/htdocs/spotHistory/getSpots.php
+++ Website/htdocs/spotHistory/getSpots.php
@@ -2,23 +2,24 @@
 require "../src/connect.php";
 
 function getTemp($spotID){
+  require "../src/connect.php";
+  $temp = "n/a";
 
-$temp = "n/a";
+  $statement = "SELECT T.temperature FROM temperaturelog as T
+  ORDER BY date ASC
+  WHERE spotID = $spotID
+  " ;
 
-$statement = "SELECT T.temperature FROM temperaturelog as T
-			ORDER BY date ASC
-			WHERE spotID = $spotID
-" ;
-
-$result = $conn->query($statement);
-
-if ($result->num_rows > 0)
-{
-	$row = $result->fetch_assoc();
-	
-	$temp = $row[temperature];
-}
-return $temp;
+  $result = $conn->query($statement);
+  
+  if ($result->num_rows > 0)
+  {
+    $row = $result->fetch_assoc();
+    
+    $temp = $row[temperature];
+  }
+  $conn->close();
+  return $temp;
 }
 
 $spots = "";
