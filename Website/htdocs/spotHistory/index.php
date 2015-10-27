ab82b1015cc4d5c043fc41868e728f4bec13394b
InghamNick <inghamnick@yahoo.co.uk>
2015-10-27 18:02:13 +0000

    fixed runtime errors

--- Website/htdocs/spotHistory/index.php
+++ Website/htdocs/spotHistory/index.php
@@ -1,17 +1,37 @@
 <?php
 $title = "Spot History";
   $pageName = "Spots";
-	$path = "../src/templates/";
-	include $path."main.php";
 ?>
 
 <!DOCTYPE html>
 
 <html>
-	<script src='<?php echo $path; ?>../bower_components/jquery/dist/jquery.min.js'></script>
-	<link href='<?php echo $path; ?>../bower_components/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet'>
-	
-	<title>sunspots</title>
+  <head>
+		<meta charset='utf-8'>
+		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+		<title><?php echo $title; ?></title>
+
+		<link rel='shortcut icon' href='<?php echo $path; ?>../images/TabLogo.png'>
+		<script src='<?php echo $path; ?>../bower_components/jquery/dist/jquery.min.js'></script>
+		<link href='<?php echo $path; ?>../bower_components/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../bower_components/metisMenu/dist/metisMenu.min.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../dist/css/timeline.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../dist/css/sb-admin-2.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../bower_components/morrisjs/morris.css' rel='stylesheet'>
+		<link href='<?php echo $path; ?>../bower_components/font-awesome/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+		<link href='<?php echo $path; ?>../datepicker/datepicker.css' rel='stylesheet'>
+		<script src='<?php echo $path; ?>../bower_components/bootstrap/dist/js/bootstrap.min.js'></script>
+		<script src='<?php echo $path; ?>../bower_components/metisMenu/dist/metisMenu.min.js'></script>
+		<script src='<?php echo $path; ?>../dist/js/sb-admin-2.js'></script>
+		<script src='<?php echo $path; ?>../datepicker/bootstrap-datetimepicker.js'></script>
+		<script src='<?php echo $path; ?>../datepicker/bootstrap-datepicker.js'></script>
+		<script src='<?php echo $path; ?>../js/sortable.js'></script>
+		<script src='<?php echo $path; ?>../js/jquery.bind.sortable.js'></script>
+		<script src='<?php echo $path; ?>../js/annyang.min.js'></script>
+		<link href='<?php echo $path; ?>../css/easyLiving.css' rel='stylesheet'>
+		<script src='<?php echo $path; ?>../templates/main.js'></script>
 	
 	<div class="row">
         <div class="col-lg-12">
