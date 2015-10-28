b0a1581fe6aa15b2b9e87af08f4461cb5e65e72a
InghamNick <inghamnick@yahoo.co.uk>
2015-10-28 20:00:29 +0000

    make modal disappear

--- Website/htdocs/spotHistory/index.php
+++ Website/htdocs/spotHistory/index.php
@@ -35,24 +35,27 @@ $title = "Spot History";
 				  ?>
 			</div>
 		</div>
+	</div>
+	
+	<div class="modal fade" id="myModal" role="dialog">
+		<div class="modal-dialog">
 		
-		<div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
+			<div class="modal-content">
+				<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Modal Header</h4>
+				</div>
+				<div class="modal-body">
+				<p>Some text in the modal.</p>
+				</div>
+				<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
       
+		</div>
     </div>
-  </div>
   
-</div>
 		
 	</div>
 	
