84fe121c03a685736baea2b0595a1c1eaf0c9132
InghamNick <inghamnick@yahoo.co.uk>
2015-10-28 20:30:15 +0000

    proving that each button has a modal

--- Website/htdocs/spotHistory/getSpots.php
+++ Website/htdocs/spotHistory/getSpots.php
@@ -52,7 +52,7 @@ if ($result->num_rows > 0)
 			<div class='modal-content'>
 				<div class='modal-header'>
 				<button type='button' class='close' data-dismiss='modal'>&times;</button>
-				<h4 class='modal-title'>Modal Header</h4>
+				<h4 class='modal-title'>$row[spotID]</h4>
 				</div>
 				<div class='modal-body'>
 				<p>Some text in the modal.</p>
