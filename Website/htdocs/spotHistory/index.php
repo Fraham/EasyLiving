4deee6a428bd83a7412ce8d6a20a183c229fe99c
InghamNick <inghamnick@yahoo.co.uk>
2015-10-28 19:46:09 +0000

    trying to sort formatting

--- Website/htdocs/spotHistory/index.php
+++ Website/htdocs/spotHistory/index.php
@@ -7,30 +7,11 @@ $title = "Spot History";
 
 <html>
   <head>
-		<meta charset='utf-8'>
-		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-		<meta name='viewport' content='width=device-width, initial-scale=1'>
-
-		<title><?php echo $title; ?></title>
-
-		<link rel='shortcut icon' href='<?php echo $path; ?>../images/TabLogo.png'>
-		<script src='<?php echo $path; ?>../src/bower_components/jquery/dist/jquery.min.js'></script>
-		<link href='<?php echo $path; ?>../src/bower_components/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet'>
-		<link href='<?php echo $path; ?>../src/bower_components/metisMenu/dist/metisMenu.min.css' rel='stylesheet'>
-		<link href='<?php echo $path; ?>../src/dist/css/timeline.css' rel='stylesheet'>
-		<link href='<?php echo $path; ?>../src/dist/css/sb-admin-2.css' rel='stylesheet'>
-		<link href='<?php echo $path; ?>../src/bower_components/morrisjs/morris.css' rel='stylesheet'>
-		<link href='<?php echo $path; ?>../src/bower_components/font-awesome/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
-		<link href='<?php echo $path; ?>../src/datepicker/datepicker.css' rel='stylesheet'>
-		<script src='<?php echo $path; ?>../src/bower_components/bootstrap/dist/js/bootstrap.min.js'></script>
-		<script src='<?php echo $path; ?>../src/bower_components/metisMenu/dist/metisMenu.min.js'></script>
-		<script src='<?php echo $path; ?>../src/dist/js/sb-admin-2.js'></script>
-		<script src='<?php echo $path; ?>../src/datepicker/bootstrap-datetimepicker.js'></script>
-		<script src='<?php echo $path; ?>../src/datepicker/bootstrap-datepicker.js'></script>
-		<script src='<?php echo $path; ?>../src/js/sortable.js'></script>
-		<script src='<?php echo $path; ?>../src/js/jquery.bind.sortable.js'></script>
-		<script src='<?php echo $path; ?>../src/js/annyang.min.js'></script>
-		<link href='<?php echo $path; ?>../src/css/easyLiving.css' rel='stylesheet'>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		
 	</head>
 	<body>
